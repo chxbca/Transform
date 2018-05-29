bbaac6ea8077d4ede30397c667fbada1a86a7838
chxbca <chxbca@gmail.com>
2018-05-29 23:02:19 +0800

    init

--- src/com/company/XML2Prop.java
+++ src/com/company/XML2Prop.java
@@ -27,6 +27,7 @@ class XML2Prop {
     void setFile(File file) throws DocumentException {
         this.file = file;
         createXML();
+        updateAllValue();
     }
 
     void setFile(String filePath) throws DocumentException {
@@ -37,7 +38,6 @@ class XML2Prop {
         SAXReader reader = new SAXReader();
         Document document = reader.read(file);
         root = document.getRootElement();
-        updateAllValue();
     }
 
     void updateAllValue() {
